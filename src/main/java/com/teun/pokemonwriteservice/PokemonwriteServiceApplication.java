9bf824364a04848cac25661081bb6013f7312b38
Teun <teunbuis17@gmail.com>
2023-06-13 14:17:23 +0200

    Init

---
+++ src/main/java/com/teun/pokemonwriteservice/PokemonwriteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.teun.pokemonwriteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonwriteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonwriteServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teun/pokemonwriteservice/PokemonwriteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teun.pokemonwriteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonwriteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
